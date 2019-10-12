dec29a4c40864145ec5e06a4e85068400c30bb65
nghichcode <nghichcode@gmail.com>
2019-10-12 22:20:16 +0700

    Add static

---
+++ static/index.jsx
@@ -0,0 +1,28 @@
+class Welcome extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {date : new Date()};
+	}
+
+	componentDidMount(){
+		this.timerID = setInterval(()=>{this.tick();},1000)
+	}
+
+	componentWillUnmount(){
+		clearInterval(this.timerID);
+	}
+
+	tick(){ this.setState({date:new Date()}); }
+
+	render() {
+		return (
+			<div>
+		    <h1>Hello, {this.props.name}!</h1>
+		    <h2>It is {this.state.date.toLocaleString()}.</h2>
+			</div>
+		);
+	}	
+}
+
+ReactDOM.render(<Welcome name="Sara" />, document.getElementById('root'));
+
